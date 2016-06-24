6d9afeba47c84a433c64cffcef13b33eff6a3aab
Albert D Yang <Napoleon356977480@gmail.com>
2016-06-25 07:35:29 +1000

    build

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import os, sys, multiprocessing, subprocess
+from build_util import *
 
 def exec_command_line(content):
 	p = subprocess.Popen(content, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -6,31 +7,44 @@ def exec_command_line(content):
 		str_line = line.decode("utf-8")
 		sys.stdout.write("	" + line)
 
-try:
-	import build_cfg
-except:
-	print("Generating build_cfg.py ...")
-	import shutil
-	shutil.copyfile("build_cfg_default.py", "build_cfg.py")
-	import build_cfg
+if __name__ == "__main__":
+	cfg = cfg_from_argv(sys.argv)
+	bi = build_info(cfg.compiler, cfg.archs, cfg.cfg)
+	print("Starting build project: " + build_cfg.project_name + " ...")
+	print("Downloading dependents ...")
 
-print("Starting build project: " + build_cfg.project_name + " ...")
-print("Downloading dependents ...")
+	if not os.path.isdir(build_cfg.dependent_path):
+		print("Making directory: " + build_cfg.dependent_path + " ...")
+		os.makedirs(build_cfg.dependent_path)
 
-if not os.path.isdir(build_cfg.dependent_path):
-	print("Making directory: " + build_cfg.dependent_path + " ...")
-	os.makedirs(build_cfg.dependent_path)
+	curdir = os.path.abspath(os.curdir)
 
-curdir = os.path.abspath(os.curdir)
+	for dependent in build_cfg.dependent_list:
+		if os.path.isdir(build_cfg.dependent_path + "/" + dependent[0]):
+			os.chdir(curdir + "/" + build_cfg.dependent_path + "/" + dependent[0])
+			cmake_cmd = batch_command(bi.host_platform)
+			cmake_cmd.add_command("git pull")
+			if cmake_cmd.execute() != 0:
+				log_error("Config %s failed." % build_cfg.project_name)
+		else:
+			os.chdir(curdir + "/" + build_cfg.dependent_path)
+			print("Cloning " + dependent[0] + ":")
+			cmake_cmd = batch_command(bi.host_platform)
+			cmake_cmd.add_command("git clone " + dependent[1] + " " + dependent[0])
+			if cmake_cmd.execute() != 0:
+				log_error("Config %s failed." % build_cfg.project_name)
+		os.chdir(curdir)
 
-for dependent in build_cfg.dependent_list:
-	if os.path.isdir(build_cfg.dependent_path + "/" + dependent[0]):
-		os.chdir(curdir + "/" + build_cfg.dependent_path + "/" + dependent[0])
-		print("pull " + dependent[0] + ":")
-		exec_command_line("git pull")
-	else:
-		os.chdir(curdir + "/" + build_cfg.dependent_path)
-		print("clone " + dependent[0] + ":")
-		exec_command_line("git clone " + dependent[1] + " " + dependent[0])
-	os.chdir(curdir)
+	additional_options = "-DCFG_PROJECT_NAME:STRING=\"%s\"" % build_cfg.project_name
+	additional_options += " -DCFG_BINARY_PATH:STRING=\"%s\"" % build_cfg.binary_path
+	additional_options += " -DCFG_BUILD_PATH:STRING=\"%s\"" % build_cfg.build_path
+	additional_options += " -DCFG_DEPENDENT_PATH:STRING=\"%s\"" % build_cfg.dependent_path
+	additional_options += " -DCFG_DOCUMENT_PATH:STRING=\"%s\"" % build_cfg.document_path
+	additional_options += " -DCFG_EXTERNAL_PATH:STRING=\"%s\"" % build_cfg.external_path
+	additional_options += " -DCFG_INCLUDE_PATH:STRING=\"%s\"" % build_cfg.include_path
+	additional_options += " -DCFG_SOURCE_PATH:STRING=\"%s\"" % build_cfg.source_path
+	additional_options += " -DCFG_TEST_PATH:STRING=\"%s\"" % build_cfg.test_path
 
+	print("Building %s..." % (build_cfg.project_name))
+	for info in bi.compilers:
+		build_project(build_cfg.project_name, build_cfg.build_path, bi, info, info.is_windows, additional_options)
